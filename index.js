af0be2a18b9a745800e00e3f7c0841912f7b086b
Azerlus <35220586+Azerlus@users.noreply.github.com>
2020-11-13 03:42:16 +0100

    0.1
    gitignore added
    the bot can read messages
    !ping command

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
-
 const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+    if (message.content === '!ping') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('Pong.');
+    }
+	console.log(message.content);
+});
+
+
 
 client.login(config.BOT_TOKEN);
