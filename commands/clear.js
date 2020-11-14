05e372a1102c999d8c21016fb7eb5d26ac7601b3
Azerlus <35220586+Azerlus@users.noreply.github.com>
2020-11-14 04:54:36 +0100

    0.2.1
    +help command
    +userInfo command
    +reload command
    +clear command

---
+++ commands/clear.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: 'clear',
+    description: 'Clear up to 100 messages.',
+    allias: ['prune'],
+    usage: ['number of messages between 1 and 99'],
+    execute(message, args) {
+        const amount = parseInt(args[0]) + 1;
+
+        if(isNaN(amount))
+            return message.reply('You need to enter a valid number.');
+        else if (amount <= 1 || amount > 100)
+            return message.reply('You need to enter a number between 1 and 99.');
+
+        message.channel.bulkDelete(amount, true).catch(err => {
+            console.log(err);
+            message.channel.send('There was an error trying to clear messages in this channel!');
+        });
+    },
+}
---
+++ commands/userinfo.js
@@ -0,0 +1,24 @@
+module.exports = {
+    name: 'userinfo',
+    description: 'Send details of a user.',
+    aliases: ['user'],
+    usage: ['user'],
+	execute(message) {
+        const data = [];
+		if (!message.mentions.users.size) {
+            data.push('Here are your infos');
+            data.push(`ID: ${message.author.id}`);
+            data.push(`Username: ${message.author.username}`);
+            message.channel.send(message.author.displayAvatarURL({dynamic: true}));
+            return message.channel.send(data, { split: true})
+        }
+
+        const avatarList = message.mentions.users.map(user => {
+            data.push(`Here are ${user.username}'s info:`);
+            data.push(`ID: ${user.id}`);
+            data.push(`Username: ${user.username}`);
+            message.channel.send(user.displayAvatarURL({dynamic: true}));
+            return message.channel.send(data, {split: true})
+        });
+	},
+};
